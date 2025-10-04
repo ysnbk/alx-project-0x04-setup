e242474e4809a6c63eca66202de697b7b1ce4104
ysnbk <130864850+ysnbk@users.noreply.github.com>
2025-10-04 23:58:57 +0000

    task 2

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -2,8 +2,8 @@ import { createContext, useContext,  useState, ReactNode } from "react"
 
 interface CountContextProps {
   count: number
-  increment: () => void
-  decrement: () => void
+  increment: () => void;
+  decrement: () => void;
 }
 
 export const CountContext = createContext<CountContextProps | undefined>(undefined)
