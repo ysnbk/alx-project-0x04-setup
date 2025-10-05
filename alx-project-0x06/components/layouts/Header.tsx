49b5a5a2054f39b776659c671d89fa6ccc1ecc0e
ysnbk <130864850+ysnbk@users.noreply.github.com>
2025-10-05 00:07:07 +0000

    task 3

--- alx-project-0x06/components/layouts/Header.tsx
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import Button from "../common/Button";
 import { usePathname } from "next/navigation";
-import { useCount } from "@/context/CountContext"";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 
 const Header: React.FC = () => {
 
   const pathname = usePathname()
-  const { count } = useCount()
+  const count = useSelector((state: RootState) => state.counter.value)
+
 
   return (
     <header className="fixed w-full bg-white shadow-md">
