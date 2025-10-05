f23603985fef92e16b23dd2ad385ae978736ab28
ysnbk <130864850+ysnbk@users.noreply.github.com>
2025-10-05 00:02:50 +0000

    task 3

---
+++ alx-project-0x06/interface/index.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+export interface ButtonProps {
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
+}
+export interface PageRouteProps {
+  pageRoute: string
+}
+export interface LayoutProps {
+  children: ReactNode;
+}
